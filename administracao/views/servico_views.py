6112f45dd5f5c3dcc4946bea43db6c9ef4754caa
Igor Gledson <igorgledson504@gmail.com>
2025-06-18 10:57:49 -0300

    Cadastro de usuários finalizado

--- administracao/views/servico_views.py
+++ administracao/views/servico_views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import ServicoForm
-from .models import Servico
+from ..forms.servico_forms import ServicoForm
+from ..models import Servico
 
 def cadastrar_servico(request):
     if request.method == "POST":
         form_servico = ServicoForm(request.POST)
-        if form_servico.is_valid():
+        if form_servico.is_valid(): 
             form_servico.save()
             return redirect('listar_servicos')
     else:     
